01654a3c3d8331148732017ab43f3ba786e4d1bd
yuanfan <1016127734@qq.com>
2017-11-23 09:11:00 +0800

    [增加]获取mac地址工具类

---
+++ src/com/lazada/util/IpMacUtil.java
@@ -0,0 +1,38 @@
+package com.lazada.util;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+public class IpMacUtil {
+	
+	
+	 //获取MAC地址的方法  
+    public static String getMACAddress(){  
+    	
+    	
+		try {
+			InetAddress ia = InetAddress.getLocalHost();//获取本地IP对象  
+			byte[] mac= NetworkInterface.getByInetAddress(ia).getHardwareAddress(); //获得网络接口对象（即网卡），并得到mac地址，mac地址存在于一个byte数组中。  
+			//下面代码是把mac地址拼装成String  
+	        StringBuffer sb = new StringBuffer();  
+	          
+	        for(int i=0;i<mac.length;i++){  
+	            if(i!=0){  
+	                sb.append("-");  
+	            }  
+	            //mac[i] & 0xFF 是为了把byte转化为正整数  
+	            String s = Integer.toHexString(mac[i] & 0xFF);  
+	            sb.append(s.length()==1?0+s:s);  
+	        }  
+	        //把字符串所有小写字母改为大写成为正规的mac地址并返回  
+	        return sb.toString().toUpperCase();  
+		} catch (UnknownHostException | SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		  }     
+      return "";
+          
+    }  
+}
