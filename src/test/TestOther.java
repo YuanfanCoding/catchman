0629574191ce3161515001de0e418e5589889f0d
yuanfan <1016127734@qq.com>
2017-11-25 19:50:25 +0800

    [新增、改进]卖点代码保留  去掉好评括号

---
+++ src/test/TestOther.java
@@ -0,0 +1,10 @@
+package test;
+
+public class TestOther {
+
+		public static void main(String[] args) throws Exception {  
+		    System.out.println("abc".replace("(", "").replace(")", ""));  
+		}  
+	
+
+}
