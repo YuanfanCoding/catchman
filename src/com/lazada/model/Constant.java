822e9e974fd074fcbf7d976e1b90a911a2c8bf15
yuanfan <1016127734@qq.com>
2017-11-25 22:13:39 +0800

    [增加]获取试用版记录api

--- src/com/lazada/model/Constant.java
+++ src/com/lazada/model/Constant.java
@@ -11,13 +11,16 @@ public class Constant {
 	public static String MACADDDRESS=IpMacUtil.getMACAddress();
 	
 	public  static final  String WEBSITE="http://ylfcoding.cn";
+	public  static final  String HTTPADDR="http://localhost:8080";
 	public  static final  String ADVERTISEMENT="http://ylfcoding.cn";
 	public final static String VERSION="V1.0";
 	
 	
 	//http«Î«Û
-	public final static String GETUSER = "http://localhost:8080/V1/user";
-	public final static String ADDUSER = "http://localhost:8080/V1/user";
-	public final static String GETVERSION = "http://localhost:8080/version";
-	public final static String UPDATEUSER = "http://localhost:8080/V1/user/update";
+	public final static String GETUSER = HTTPADDR+"/V1/user";
+	public final static String ADDUSER = HTTPADDR+"/V1/user";
+	public final static String GETVERSION = HTTPADDR+"/version";
+	public final static String UPDATEUSER = HTTPADDR+"/V1/user/update";
+	public final static String GETRECORD = HTTPADDR+"/testrecord";
+	
 }
